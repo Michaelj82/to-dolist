765ed7964ea41b6af22d538432032dc118efa5d0
Michael <mjj.yoyogi@gmail.com>
2023-02-03 23:48:34 -0500

    test git.

--- src/dommanipulation.js
+++ src/dommanipulation.js
@@ -13,8 +13,7 @@ export const content = document.getElementById('content');
 
 //login box 
 let loginMaker = document.createElement('div');
-
-
+let signIn = document.createElement('button')
 
 
 //create left side of header
